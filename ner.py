a374c112362e7b9473e707a49a0f66b23429b0d1
Ubuntu <saeednjf@saeednjf-deep-ubuntu.ee3hbcfdn0ae5myv3t3s5sjyph.bx.internal.cloudapp.net>
2017-08-14 07:05:05 +0000

    updating

--- ner.py
+++ ner.py
@@ -29,14 +29,14 @@ class NER(object):
     early_stopping = 2
 
     """inference type"""
-    #inference = "softmax"
+    inference = "softmax"
     #inference = "crf"
-    inference = "decoder_rnn"
+    #inference = "decoder_rnn"
 
     """for decoder_rnn"""
     #decoding="greedy"
-    decoding="beamsearch"
-    beamsize=4
+    #decoding="beamsearch"
+    #beamsize=12
     #decoding="viterbi"
 
 
