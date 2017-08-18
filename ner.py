0d7c8c501580db475066575d14edef5b203c7feb
SaeedNajafi <snajafi@ualberta.ca>
2017-08-18 16:25:07 -0600

    indentation fixed

--- ner.py
+++ ner.py
@@ -267,10 +267,10 @@ def run_NER():
             print 'Epoch {}'.format(epoch)
             start = time.time()
             ###
-	    if(epoch==1 or epoch==6 or epoch==12 or epoch==20):
-		optimizer_scope = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                                 "adam_optimizer")
-		session.run(tf.variables_initializer(optimizer_scope))
+            if(epoch==1 or epoch==6 or epoch==12 or epoch==20):
+                optimizer_scope = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "adam_optimizer")
+                session.run(tf.variables_initializer(optimizer_scope))
+            
             train_loss = run_epoch(
                                     config,
                                     model,
