49629f23de1c56b40ac82cf8fcaa49c8a0b5ba4b
SaeedNajafi <snjf@Saeeds-MacBook-Pro.local>
2018-01-13 15:58:47 -0700

    differential scheduled sampling added

--- model.py
+++ model.py
@@ -531,7 +531,7 @@ class NER(object):
         return self.loss
 
 
-    def train_by_scheduled_decoder_rnn(H, config):
+    def train_by_scheduled_decoder_rnn(self, H, config):
         b_size = tf.shape(H)[0]
 
         #we need to define a tag embedding layer.
@@ -580,10 +580,15 @@ class NER(object):
         GO_symbol = tf.zeros((b_size, config.tag_embedding_size), dtype=tf.float32)
         GO_context = tf.zeros((b_size, config.word_rnn_hidden_units), dtype=tf.float32)
 
+        cond = tf.greater(self.coin_probs_placeholder, self.schedule_placeholder)
+        switch = tf.cast(cond, dtype=tf.float32)
+        #0 for gold previous token, 1 for generated previous token
+        switch_t = tf.transpose(switch, [1,0])
+
         tag_embeddings_t = tf.transpose(tag_embeddings, [1,0,2])
         H_t = tf.transpose(H, [1,0,2])
         Logits = []
-        with tf.variable_scope('decoder_rnn', reuse=True) as scope:
+        with tf.variable_scope('decoder_rnn') as scope:
             initial_state = self.decoder_lstm_cell.zero_state(b_size, tf.float32)
             for time_index in range(config.max_sentence_length):
                 if time_index==0:
@@ -597,23 +602,21 @@ class NER(object):
                 H_and_output = tf.concat([H_t[time_index], output_dropped], axis=1)
                 logits = tf.add(tf.matmul(H_and_output, W_softmax), b_softmax)
                 Logits.append(logits)
-                def gold_token():
-                    #gold previous token
-                    prev_output = tag_embeddings_t[time_index-1]
-                    return tf.concat([prev_output, H_t[time_index-1]], axis=1)
-                def generated_token():
-                    #generated previous token
-                    #approximating argmax in finding the token with the highest probability.
-                    # in our case self.schedule_placeholder will be changed between
-                    # 1.0 to 0.8. So we map it to 1.0 to 6.0.
-                    # so we increase beta from 10^1 to 10^6.
-                    beta = -25*(self.schedule_placeholder-0.8) + 6.0
-                    beta = 10**beta
-                    prev_output = tf.matmul(tf.nn.softmax(beta * logits), tag_lookup_table)
-                    return tf.concat([prev_output, H_t[time_index-1]], axis=1)
 
-                cond = tf.greater(self.coin_probs_placeholder, self.schedule_placeholder)
-                inp = tf.cond(cond, generated_token, gold_token)
+                gold_token = tf.concat([tag_embeddings_t[time_index-1], H_t[time_index-1]], axis=1)
+
+                #generated previous token
+                #approximating argmax in finding the token with the highest probability.
+                # in our case self.schedule_placeholder will be changed between
+                # 1.0 to 0.8. So we map it to 1.0 to 6.0.
+                # so we increase beta from 10^1 to 10^6.
+                beta = -25*(self.schedule_placeholder-0.8) + 6.0
+                beta = 10**beta
+                prev_output = tf.matmul(tf.nn.softmax(beta * logits), tag_lookup_table)
+                generated_token = tf.concat([prev_output, H_t[time_index-1]], axis=1)
+
+                sw = switch_t[time_index]
+                inp = sw * generated_token + (1-sw) * gold_token
 
         Logits = tf.stack(Logits, axis=1)
         cross_loss = tf.contrib.seq2seq.sequence_loss(
